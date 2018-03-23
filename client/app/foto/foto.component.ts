bba557b1d064ba6e89e3c28f7b9636b4a1092ca2
Tiago dos Santos Querubim <tiago.querubim@miranteinfo.com>
2018-03-23 16:11:14 -0300

    initial commit

---
+++ client/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core'
+@Component({
+    moduleId: module.id,
+    selector: 'foto',
+    templateUrl: './foto.component.html'
+})
+export class FotoComponent {
+    @Input() url;
+    @Input() titulo;
+}
