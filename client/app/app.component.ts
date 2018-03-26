a5af23819e7f77d5be3d22a752ef3ddf396a32ae
= <tquerubim@gmail.com>
2018-03-25 22:49:00 -0300

    adicionando chamada a api de fotos

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-}
+    fotos: Object[] = [];
+
+    constructor(http: Http) {
+
+        http
+            .get('v1/fotos')
+            .map(res => res.json())
+            .subscribe(res => {
+                this.fotos = res;
+            }, erro => console.log('erro'))
+
+    }
+} 
